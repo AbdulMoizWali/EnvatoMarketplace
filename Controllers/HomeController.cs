243fee76938cce2f796499230ba42d73f35ede1e
AbdulMoiz <85098925+AbdulMoizWali@users.noreply.github.com>
2023-12-29 21:18:24 +0500

    feat: Category Create tested

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,8 +29,41 @@ namespace EnvatoMarketplace.Controllers
                 ViewBag.rid = new SelectList(db.Roles, "rid", "name", user.rid);
                 db.Users.Add(user);
                 db.SaveChanges();
+                return RedirectToAction("Index");
             }
             return View(user);
         }
+
+        public ActionResult Categories() {
+            return View(db.Categories.ToList());
+        }
+
+        public ActionResult CreateCategory()
+        {
+            ViewBag.pcatid = new SelectList(db.Categories, "catid", "catName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategory(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Categories.Add(category);
+                    db.SaveChanges();
+                    return RedirectToAction("Categories");
+                }
+                catch (Exception ex)
+                {
+                    // Log the error (uncomment ex variable name and write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            ViewBag.pcatid = new SelectList(db.Categories, "catid", "catName", category.pcatid);
+            return View(category);
+        }
+
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -17,7 +17,7 @@ namespace EnvatoMarketplace.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Category()
         {
-            this.Category1 = new HashSet<Category>();
+            this.Categories = new HashSet<Category>();
             this.Products = new HashSet<Product>();
         }
     
@@ -26,8 +26,8 @@ namespace EnvatoMarketplace.Models
         public Nullable<int> pcatid { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Category> Category1 { get; set; }
-        public virtual Category Category2 { get; set; }
+        public virtual ICollection<Category> Categories { get; set; }
+        public virtual Category ParentCategory { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Product> Products { get; set; }
     }
