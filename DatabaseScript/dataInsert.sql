8bf99559803924f8bbaf3b1d5e4bd3d251e1c511
AbdulMoiz <85098925+AbdulMoizWali@users.noreply.github.com>
2023-12-29 17:43:22 +0500

    feat: Added Data

--- DatabaseScript/dataInsert.sql
+++ DatabaseScript/dataInsert.sql
@@ -1,8 +1,15 @@
 BEGIN TRANSACTION;
 BEGIN TRY
 
+DBCC CHECKIDENT ('Category', RESEED, 0);
+DBCC CHECKIDENT ('User', RESEED, 0);
+DBCC CHECKIDENT ('Logs', RESEED, 0);
+DBCC CHECKIDENT ('Product', RESEED, 0);
+DBCC CHECKIDENT ('Cart', RESEED, 0);
+DBCC CHECKIDENT ('Review', RESEED, 0);
+
 -- Insert into Category
-INSERT INTO [Category] ([catName], [pcatid]) VALUES ('Electronics', NULL);
+INSERT INTO [Category] ([catName]) VALUES ('Electronics');
 INSERT INTO [Category] ([catName], [pcatid]) VALUES ('Mobiles', 1);
 
 -- Insert into Role
@@ -12,9 +19,13 @@ INSERT INTO [Role] ([rid], [name]) VALUES (3, 'Customer');
 
 -- Insert into User
 INSERT INTO [User] ([name], [profilePic], [username], [password], [dob], [roleid]) 
-VALUES ('Abdul Moiz', '~/uploads/userImages/user.png', 'Abdul Moiz', 'Abdul Moiz', '1980-01-01', 3);
+VALUES ('Admin', '~/uploads/userImages/user.png', 'Admin', 'Admin', '2000-01-01', 1);
 INSERT INTO [User] ([name], [profilePic], [username], [password], [dob], [roleid]) 
-VALUES ('Ahmed Zaman', '~/uploads/userImages/user.png', 'Ahmed Zaman', 'Ahmed Zaman', '1985-01-01', 3);
+VALUES ('Vendor', '~/uploads/userImages/user.png', 'Vendor', 'Vendor', '2000-01-01', 2);
+INSERT INTO [User] ([name], [profilePic], [username], [password], [dob], [roleid]) 
+VALUES ('Abdul Moiz', '~/uploads/userImages/user.png', 'Abdul Moiz', 'Abdul Moiz', '2000-01-01', 3);
+INSERT INTO [User] ([name], [profilePic], [username], [password], [dob], [roleid]) 
+VALUES ('Ahmed Zaman', '~/uploads/userImages/user.png', 'Ahmed Zaman', 'Ahmed Zaman', '2000-01-01', 3);
 
 -- Insert into Logs
 INSERT INTO [Logs] ([uid]) VALUES (1);
@@ -22,17 +33,17 @@ INSERT INTO [Logs] ([uid]) VALUES (2);
 
 -- Insert into Product
 INSERT INTO [Product] ([uid], [name], [productPic], [shortDesc], [longDesc], [amount], [availableQty], [catid]) 
-VALUES (1, 'iPhone 13', '~/uploads/productImages/iphone13.jpg', 'Latest iPhone 13', 'The iPhone 13 is the latest smartphone from Apple.', 999.00, 10, 2);
+VALUES (1, 'iPhone 13', '~/uploads/productImages/iphone13.jpeg', 'Latest iPhone 13', 'The iPhone 13 is the latest smartphone from Apple.', 999.00, 10, 2);
 INSERT INTO [Product] ([uid], [name], [productPic], [shortDesc], [longDesc], [amount], [availableQty], [catid]) 
-VALUES (2, 'Samsung Galaxy S21', '~/uploads/productImages/galaxys21.jpg', 'Latest Samsung Galaxy S21', 'The Samsung Galaxy S21 is the latest smartphone from Samsung.', 899.00, 20, 2);
+VALUES (2, 'Samsung Galaxy S21', '~/uploads/productImages/galaxys21.jpeg', 'Latest Samsung Galaxy S21', 'The Samsung Galaxy S21 is the latest smartphone from Samsung.', 899.00, 20, 2);
 
 -- Insert into Payment
 INSERT INTO [Payment] ([payid], [type]) VALUES (1, 'Cash');
 INSERT INTO [Payment] ([payid], [type]) VALUES (2, 'Card');
 
 -- Insert into Cart
-INSERT INTO [Cart] ([uid]) VALUES (1);
-INSERT INTO [Cart] ([uid]) VALUES (2);
+INSERT INTO [Cart] ([uid]) VALUES (3);
+INSERT INTO [Cart] ([uid]) VALUES (4);
 
 -- Insert into Review
 INSERT INTO [Review] ([pid], [comment], [rating]) VALUES (1, 'Great phone with latest features.', 5);
@@ -45,13 +56,14 @@ INSERT INTO [CartItem] ([cid], [pid], [pName], [pShortDesc], [pAmount], [pBuyQty
 VALUES (2, 2, 'Samsung Galaxy S21', 'Latest Samsung Galaxy S21', 899.00, 1);
 
 
- COMMIT;
+-- COMMIT;
 END TRY
 BEGIN CATCH
     ROLLBACK;
     DECLARE @ErrorMessage NVARCHAR(4000);
     DECLARE @ErrorSeverity INT;
     DECLARE @ErrorState INT;
+	print('Rollbacked');
     SELECT 
         @ErrorMessage = ERROR_MESSAGE(),
         @ErrorSeverity = ERROR_SEVERITY(),
