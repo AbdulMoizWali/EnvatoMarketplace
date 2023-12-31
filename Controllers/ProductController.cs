047de99c397f273b04eabad32d72e47a92799666
AbdulMoiz <85098925+AbdulMoizWali@users.noreply.github.com>
2023-12-31 23:52:09 +0500

    feat: Added Single Product Detail

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using EnvatoMarketplace.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnvatoMarketplace.Controllers
+{
+    public class ProductController : Controller
+    {
+        EnvatoDBEntities db = new EnvatoDBEntities();
+        // GET: Product
+        public ActionResult Index(int? id)
+        {
+            try
+            {
+                Product product = db.Products.First(prod => prod.pid == id);
+                return View(product);
+            }
+            catch(Exception ex) {
+                return RedirectToAction("NotFound", "Default");
+            }
+        }
+    }
+}
