359027246ce9a2633d087eafc2cd56ebe6c94d6a
AhmedZaman14 <zaman994ahmed@gmail.com>
2023-12-31 15:51:16 +0500

    Revert "Merge branch 'login' into dev"
    This reverts commit 4d313248d7112908f9654e347bafba810d1e6555, reversing
    changes made to b16bd8951b9217b89117f563c5b419b71fd5f5fa.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,69 @@
-﻿using System;
+﻿using EnvatoMarketplace.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace EnvatoMarketplace.Controllers
 {
     public class HomeController : Controller
     {
+        EnvatoDBEntities db = new EnvatoDBEntities();
         // GET: Home
         public ActionResult Index()
         {
+            return View(db.Users.ToList());
+        }
+
+        public ActionResult CreateUser() {
+            ViewBag.rid = new SelectList(db.Roles, "rid", "name");
             return View();
         }
+
+        [HttpPost]
+        public ActionResult CreateUser(User user)
+        {
+            if (ModelState.IsValid) {
+                ViewBag.rid = new SelectList(db.Roles, "rid", "name", user.rid);
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        public ActionResult Categories() {
+            return View(db.Categories.ToList());
+        }
+
+        public ActionResult CreateCategory()
+        {
+            ViewBag.pcatid = new SelectList(db.Categories, "catid", "catName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategory(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Categories.Add(category);
+                    db.SaveChanges();
+                    return RedirectToAction("Categories");
+                }
+                catch (Exception ex)
+                {
+                    // Log the error (uncomment ex variable name and write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            ViewBag.pcatid = new SelectList(db.Categories, "catid", "catName", category.pcatid);
+            return View(category);
+        }
+
     }
 }
