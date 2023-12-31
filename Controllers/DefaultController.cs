9e63eea430b728447f2399b89adf658518760633
AbdulMoiz <85098925+AbdulMoizWali@users.noreply.github.com>
2023-12-31 23:09:00 +0500

    feat: Added 404 Not Found

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnvatoMarketplace.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
